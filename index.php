a02246e4dc2619783d3c657a902aa3bafd78bf30
wadzinski1303shannonpaige <wadzinski1303shannonpaige@gmail.com>
2025-04-18 18:00:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,15 +6,10 @@ $accessToken = "2e8d2e0b7e4d36";
 $ip = $_SERVER['REMOTE_ADDR'];
 
 // Query IPinfo API
-$apiUrl = "https://ipinfo.io/{$ip}/json?token=2e8d2e0b7e4d36";
+$apiUrl = "https://ipinfo.io/{$ip}/json?token={$accessToken}";
 $response = @file_get_contents($apiUrl);
 $data = json_decode($response, true);
 
-// Optional: Block known bots if bot detection is enabled
-if (isset($data['bot']) && $data['bot']['is_bot'] === true) {
-    // You can log bot access or simply stop here
-    exit("Access blocked for bots.");
-}
 
 // Get country code
 $countryCode = $data['country'] ?? '';
@@ -22,7 +17,7 @@ $countryCode = $data['country'] ?? '';
 // Redirect based on country
 if ($countryCode === 'US' || $countryCode === 'CA') {
     // Redirect USA/Canada visitors to Page A
-    header("Location: https://facebook.com/");
+    header("Location: https://facebook.com");
 } else {
     // Redirect others to Page B
     header("Location: https://google.com/");
